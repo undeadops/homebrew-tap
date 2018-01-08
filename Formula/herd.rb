e0ad4d29a6d2b707aeceaf4cc1f9e0d99b0c657a
goreleaserbot <goreleaser@carlosbecker.com>
2018-01-08 14:34:19 -0700

    herd version 0.1.5

--- Formula/herd.rb
+++ Formula/herd.rb
@@ -1,9 +1,9 @@
 class Herd < Formula
   desc "AWS CLI for getting to the most needed info I use everyday but dont want to open the AWS Console to look up."
   homepage "https://github.com/undeadops/herd"
-  url "https://github.com/undeadops/herd/releases/download/0.1.4/herd_0.1.4_darwin_amd64.tar.gz"
-  version "0.1.4"
-  sha256 "92d3f88f89930444c4c3bbd37b6f2c3dfdd3eb8649a959b04298175128873dfd"
+  url "https://github.com/undeadops/herd/releases/download/0.1.5/herd_0.1.5_darwin_amd64.tar.gz"
+  version "0.1.5"
+  sha256 "cd692401867b4e2b7f38fbe61ee2463b27f4be4c34061e7d77195121aaaad2c1"
 
   def install
     bin.install "herd"
